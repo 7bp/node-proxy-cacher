47ee1a8d52f0714756862bd5fa51f51a78eba3c2
7bp <bp@7bp.de>
2013-05-11 02:05:49 +0200

    Added cache identifier filter

--- proxy.js
+++ proxy.js
@@ -4,11 +4,19 @@ var http = require('http'),
   crypto = require('crypto');
 
 if (process.argv.length <= 2) {
-  console.log('node proxy.js HOST [PORT=80] [LOCALPORT=8080]');
+  console.log('node proxy.js HOST [PORT=80] [LOCALPORT=8080] [PARAMS]');
+  console.log('HOST - remote host address');
+  console.log('PORT - remote host port');
+  console.log('LOCALPORT - local port for proxy-cache access');
+  console.log('PARAMS - if set: defines the cache identifier, delimiter = ,');
+  console.log('--');
+  console.log('Example:');
+  console.log('node proxy.js remotehost.com 80 8080 articleid,languageid');
   process.exit(0);
 }
 
 var serverPort = process.argv[4] || 8080;
+var filter = process.argv[5] || false;
 
 if (!fs.existsSync(__dirname + '/cache')) {
   fs.mkdirSync(__dirname + '/cache');
@@ -36,16 +44,37 @@ http.createServer(function(request, response) {
     method: request.method
   };
 
-  var md5Name = crypto.createHash('md5').update(request.url).digest('hex');
-  var cacheFileName = __dirname + '/cache/' + md5Name;
-  var cacheHeaderFileName = __dirname + '/cache_headers/' + md5Name;
+  /**
+  * Cache identifier will be defined. If cache identifier is empty, complete request url will be
+  * used as fallback identifier - which means: any image, or other request type will most likely 
+  * cached regarding their absolute url.
+  */
+  var cacheIdentifier = [];
+  if (filter && request.url.indexOf('?') !== -1) {
+    var path = request.url.substring(0, request.url.indexOf('?'));
+    var query = request.url.substring(path.length + 1).split('&');
+    var parameters = {};
+    for (var i = query.length - 1; i >= 0; i--) {
+      var param = query[i].split('=');
+      parameters[param[0]] = param[1];
+    }
+    cacheIdentifier.push(path);
+    for (var i = filter.length - 1; i >= 0; i--) {
+      if (parameters[filter[i]]) {
+        cacheIdentifier.push(filter[i] + '=' + parameters[filter[i]]);
+      }
+    }
+  }
+  if (cacheIdentifier.length === 0) {
+    cacheIdentifier.push(request.url);
+  }
+
+  var md5Hash = crypto.createHash('md5').update(cacheIdentifier.join('&')).digest('hex');
+  var cacheFileName = __dirname + '/cache/' + md5Hash;
+  var cacheHeaderFileName = __dirname + '/cache_headers/' + md5Hash;
 
   if (fs.existsSync(cacheFileName)) {
-    sys.log(
-      'CACHE ' +
-      request.connection.remoteAddress + ': ' + 
-      request.method + ' ' + request.url +
-      ' > ' + proxy.hostname + ':' + proxy.port + request.url);
+    sys.log('CACHE ' + cacheIdentifier.join('&'));
 
     response.writeHeader(200, JSON.parse(fs.readFileSync(cacheHeaderFileName)));
     response.write(fs.readFileSync(cacheFileName));
@@ -61,11 +90,7 @@ http.createServer(function(request, response) {
         fs.writeSync(fd, buff, 0, buff.length);
       });
       res.on('end', function() {
-        sys.log(
-          'FETCH ' +
-          request.connection.remoteAddress + ': ' + 
-          request.method + ' ' + request.url +
-          ' > ' + proxy.hostname + ':' + proxy.port + request.url);
+        sys.log('FETCH ' + cacheIdentifier.join('&') + ' - ' + request.url);
         fs.closeSync(fd);
         response.end();
       });
